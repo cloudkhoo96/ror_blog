84ddc2a25b89d15b3909f27ac7da46ea6dcb84ac
cloudkhoo96 <cloud7535@gmail.com>
2024-04-13 15:18:01 +0800

    ROR 9 Refactoring 1. Rendering Partial Collections. 2. Rendering Partial Forms. 3. Using concerns.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,12 @@
 class ArticlesController < ApplicationController
   def index
-    @articles =  Article.all
+    @articles = Article.all
   end
-  
+
   def show
     @article = Article.find(params[:id])
   end
-  
+
   def new
     @article = Article.new
   end
@@ -43,8 +43,8 @@ class ArticlesController < ApplicationController
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
 
+  def article_params
+    params.require(:article).permit(:title, :body, :status)
+  end
 end
