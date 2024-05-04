b95dd86724836052e04882cfe81fa7a82a264bed
cloudkhoo96 <cloud7535@gmail.com>
2024-05-04 12:13:55 +0800

    Refactor to show author username or "Deleted User" in articles or comments using helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
-end
+    def display_author(content)
+      author_string = "<p>Author: "
+      if content.user.nil?
+        author_string += "Deleted User"
+      else
+        author_string += content.user.username
+      end
+      author_string += "</p>"
+      author_string.html_safe
+    end
+  end
+  
