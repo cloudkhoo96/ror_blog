3b563e3e3edfe62dc543103063af055a603806b0
cloudkhoo96 <cloud7535@gmail.com>
2025-08-28 15:16:28 +0000

    Update comment ratings

--- app/controllers/comments/comment_ratings_controller.rb
+++ app/controllers/comments/comment_ratings_controller.rb
@@ -4,29 +4,15 @@ class Comments::CommentRatingsController < ApplicationController
 
   def create
     @comment.comment_ratings.create(user: current_user)
-    render_turbo_stream_update
   end
 
   def destroy
     @comment.comment_ratings.find_by(user: current_user).destroy
-    render_turbo_stream_update
   end
 
   private
 
   def set_comment
     @comment = Comment.find(params[:comment_id])
-  end
-
-  def render_turbo_stream_update
-    respond_to do |format|
-      format.turbo_stream {
-        render turbo_stream: turbo_stream.update(
-          "comment_like_count_#{@comment.id}",
-          partial: "comments/like_count",
-          locals: { comment: @comment }
-        )
-      }
-    end
   end
 end
